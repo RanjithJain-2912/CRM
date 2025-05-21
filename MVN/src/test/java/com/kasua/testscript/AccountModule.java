1b5fa66674605d9195743d4ce9b378f21d56652d
QSP <QSP@DESKTOP-3JRUFPN>
2025-05-21 11:11:49 +0530

    Open the browser

--- MVN/src/test/java/com/kasua/testscript/AccountModule.java
+++ MVN/src/test/java/com/kasua/testscript/AccountModule.java
@@ -1,11 +1,17 @@
 package com.kasua.testscript;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class AccountModule {
 	@Test
 public void demo() {
-		Reporter.log("AccountModule",true);
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.fb.com");
+		Reporter.log(driver.getTitle(),true);
+		driver.quit();
 }
 }
